ad0c165fa7abf96d97cb0e73fb0edd9ef28102b1
ROCKKKKK7 <elprimopan4@gmail.com>
2026-06-16 10:45:27 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,50 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.module.css";
 
 function App() {
-	return;
-	<>
-		<div className={styles.container}>
-			<section className={styles.topBanner}>
-				<h1 className={styles.heading}>
-					Простое приложение для прогноза погоды
-				</h1>
+	const [city, setCity] = useState("");
 
-				<form>
-					<input type="text" placeholder="Поиск города" autoFocus />
+	const handleSumbit = (e) => {
+		e.preventDefault();
+		console.log(city);
+	};
 
-					<button type="submit">ОТПРАВИТЬ</button>
+  return (
+    <div className={styles.container}>
+      <section className={styles.topBanner}>
+        <h1 className={styles.heading}>
+          Простое приложение для прогноза погоды
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Поиск города"
+            autoFocus
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button type="submit">ОТПРАВИТЬ</button>
+          <span className={styles.msg}></span>
+        </form>
+      </section>
 
-					<span className={styles.msg}></span>
-				</form>
-			</section>
-
-			<section className={styles.ajaxSection}>
-				<ul className={styles.cities}></ul>
-			</section>
-        <li class="city">
-          <h2 class="city-name" data-name="...">
-            <span>...</span>
-            <sup>...</sup>
-          </h2>
-          <span class="city-temp">...<sup>°C</sup></span>
-          <figure>
-            <img class="city-icon" src="..." alt="..."/>
-            <figcaption>...</figcaption>
-          </figure>
-        </li>
-		</div>
-	</>;
+      <section className={styles.ajaxSection}>
+        <ul className={styles.cities}>
+          <li className={styles.city}>
+            <h2 className={styles.cityName}>
+              <span>Город</span>
+              <sup>UA</sup>
+            </h2>
+            <span className={styles.cityTemp}>25<sup>°C</sup></span>
+            <figure>
+              <img className={styles.cityIcon} src="" alt="погода" />
+              <figcaption>Ясно</figcaption>
+            </figure>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 export default App;
